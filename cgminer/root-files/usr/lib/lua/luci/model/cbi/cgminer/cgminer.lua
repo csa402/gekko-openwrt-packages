08d4b0e0f1b16914ed3cc7cde7f8e0793077c73e
Your Name <you@example.com>
2021-02-22 11:17:38 +0100

    update

--- cgminer/root-files/usr/lib/lua/luci/model/cbi/cgminer/cgminer.lua
+++ cgminer/root-files/usr/lib/lua/luci/model/cbi/cgminer/cgminer.lua
@@ -28,11 +28,11 @@ pb:value("--balance", translate("Balance"))
 pb:value("--load-balance", translate("Load Balance"))
 pb:value("  ", translate("Failover"))
 
-cf1 = conf:option(Value, "cf1", translate("2pac Chip Frequency(Range: 100-250, Default: 100)"))
-cf1.default = "150"
+cf1 = conf:option(Value, "cf1", translate("compac Chip Frequency(Range: 100-250, Default: 100)"))
+cf1.default = "100"
 cf1.datatype = "range(100, 300)"
 
-cf2 = conf:option(Value, "cf2", translate("Gekko Chip Frequency(Range: 100-500, Default: 150)"))
+cf2 = conf:option(Value, "cf2", translate("2pac Chip Frequency(Range: 100-500, Default: 150)"))
 cf2.default = "150"
 cf2.datatype = "range(100, 500)"
 
